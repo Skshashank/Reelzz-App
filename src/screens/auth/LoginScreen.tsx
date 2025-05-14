c0f99c62ca5b07156d3e4df154438ffb456cdcbe
Skshashank <yadav.shashank031@gmail.com>
2025-05-15 01:54:29 +0530

    LoginScreen - gradient, Lottie animation

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -4,30 +4,77 @@ import CustomSafeAreaView from '../../components/global/CustomSafeAreaView'
 import Lottie from 'lottie-react-native'
 import Animation from '../../assets/animations/login.json'
 import { RFValue } from 'react-native-responsive-fontsize'
+import LinearGradient from 'react-native-linear-gradient'
+import { Colors } from '../../constants/Colors'
+import CustomText from '../../components/global/CustomText'
+import { FONTS } from '../../constants/Fonts'
 
 const LoginScreen = () => {
   return (
-    <CustomSafeAreaView style = {styles.container}>
-      <View style = {styles.lottieContainer}>
-        <Lottie source ={Animation} autoPlay loop style = {styles.lottie} />
+    <CustomSafeAreaView style={styles.container}>
+      <View style={styles.lottieContainer}>
+        <Lottie source={Animation} autoPlay loop style={styles.lottie} />
       </View>
-    </CustomSafeAreaView>
+
+        <View style={styles.titleContainer}>
+      <LinearGradient
+           colors={[`rgba(0,0,0,0)`, Colors.text, `rgba(0,0,0,0)`]}
+          style={styles.linearGradient}
+          start={{x: 0, y: 0.5}}
+          end={{x: 1, y: 0.5}}
+        />
+        <CustomText variant="h2" fontFamily={FONTS.Reelz}>
+          Reelzzz
+        </CustomText>
+        <LinearGradient
+          colors={[`rgba(0,0,0,0)`, Colors.text, `rgba(0,0,0,0)`]}
+          style={styles.linearGradient}
+          start={{x: 0, y: 0.5}}
+          end={{x: 1, y: 0.5}}
+        />
+      </View>
+      <CustomText variant="h6" fontFamily={FONTS.Medium} style ={styles.tagline}>
+        Rewarding Ever Moments for Creators and Viewres.
+      </CustomText>
+    </CustomSafeAreaView> 
   )
 }
 
 const styles = StyleSheet.create({
-  container : {
+  container: {
     alignItems: 'center',
     paddingTop: 40,
   },
-  lottieContainer:{
+  lottieContainer: {
     width: RFValue(220),
-    height: RFValue(220)
+    height: RFValue(220),
   },
-  lottie:{
-    width:'100%',
-    height:'100%'
-  }
-})
+  lottie: {
+    width: '100%',
+    height: '100%',
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 20,
+  },
+  linearGradient: {
+    flex: 1,
+    height: 1,
+  },
+  tagline: {
+    textAlign: 'center',
+    marginVertical: 30,
+  },
+  gimg: {
+    height: 20,
+    width: 20,
+  },
+  footerText: {
+    opacity: 0.6,
+    position: 'absolute',
+    bottom: 10,
+  },
+});
+export default LoginScreen;
 
-export default LoginScreen
