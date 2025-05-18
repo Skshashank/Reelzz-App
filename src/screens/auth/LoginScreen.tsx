16ed445c7c7822b49495e1d55c8f6e21a02e06dd
Skshashank <yadav.shashank031@gmail.com>
2025-05-19 00:47:42 +0530

    SocialButton - facebook and google login and LoginScreen changes , footer

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import CustomSafeAreaView from '../../components/global/CustomSafeAreaView'
 import Lottie from 'lottie-react-native'
@@ -8,12 +8,15 @@ import LinearGradient from 'react-native-linear-gradient'
 import { Colors } from '../../constants/Colors'
 import CustomText from '../../components/global/CustomText'
 import { FONTS } from '../../constants/Fonts'
+import SocialButtonHorizontal from '../../components/global/SocialButtonHorizontal'
+import Icon from 'react-native-vector-icons/Ionicons'
+import GoogleIcon from '../../assets/icons/google.png'
 
 const LoginScreen = () => {
   return (
     <CustomSafeAreaView style={styles.container}>
       <View style={styles.lottieContainer}>
-        <Lottie source={Animation} autoPlay loop style={styles.lottie} />
+        <Lottie source={Animation} autoPlay loop style={styles.lottie} /> 
       </View>
 
         <View style={styles.titleContainer}>
@@ -34,8 +37,31 @@ const LoginScreen = () => {
         />
       </View>
       <CustomText variant="h6" fontFamily={FONTS.Medium} style ={styles.tagline}>
-        Rewarding Ever Moments for Creators and Viewres.
+        Rewarding Ever Moments for Creators and Viewers.
       </CustomText>
+
+      <SocialButtonHorizontal
+        icon={<Icon name='logo-facebook' size={20} color={Colors.text} />}
+        onPress={async () => { }}
+        text="Continue with Facebook"
+        textColor="#fff"
+        backgroundColor={Colors.fbColor}
+      />
+
+      <SocialButtonHorizontal
+        icon={<Image source = {GoogleIcon} style = {styles.gimg} />}
+        onPress={async () => { }}
+        text="Continue with Google"
+        textColor="#000"
+        backgroundColor={Colors.white}
+      />
+      
+      <TouchableOpacity style={styles.footerText}>
+        <CustomText variant='h8' fontFamily={FONTS.Medium}>
+          Designed and developed by - Shashank Yadav
+        </CustomText>
+
+      </TouchableOpacity>
     </CustomSafeAreaView> 
   )
 }
